ae4ac86d9fddc4c995259fb0b3c76d405b26278e
Luke Kaalim <lukekaalim@gmail.com>
2021-07-26 03:39:38 +1000

    Fix NiTriShape data working the same as geometry data

--- TESVNif/Blocks/NiGeometry.cs
+++ TESVNif/Blocks/NiGeometry.cs
@@ -3,8 +3,13 @@ namespace SSE.TESVNif.Blocks
 {
     public class NiGeometry : NiAVObject
     {
+        int dataIndex;
+
+        public NiGeometryData Data => File.Objects[dataIndex] as NiGeometryData;
+
         public NiGeometry(NIFReader.NIFFile file, BlockStructure.BlockData data) : base(file, data)
         {
+            dataIndex = data.GetBasic<int>("Data");
         }
     }
 }
--- TESVNif/Blocks/NiTriShapeData.cs
+++ TESVNif/Blocks/NiTriShapeData.cs
@@ -9,18 +9,16 @@ namespace SSE.TESVNif.Blocks
     public class NiTriShapeData : NiTriBasedGeomData
     {
         public List<Triangle> Triangles { get; set; }
-        public List<BSVertexData> VertexData { get; set; }
+        public List<Vector3> Vertices { get; set; }
 
         public NiTriShapeData(NIFReader.NIFFile file, BlockStructure.BlockData data) : base(file, data)
         {
-            /*
             Triangles = data.GetCompoundList("Triangles")
                 .Select(d => new Triangle(d))
                 .ToList();
-            VertexData = data.GetCompoundList("Vertex Data")
-                .Select(d => new BSVertexData(d))
+            Vertices = data.GetCompoundList("Vertices")
+                .Select(d => new Vector3(d))
                 .ToList();
-            */
         }
     }
 }
